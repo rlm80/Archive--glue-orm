3cdad93c6d12f6da0ec7266a3a6ba5474bc09b8a
rlm80 <rlemaigr@yahoo.fr>
2009-11-29 13:07:47 +0000

    Removed query_contrib redefinition where it was not necessary.

--- classes/ogl/command/with/onetomany.php
+++ classes/ogl/command/with/onetomany.php
@@ -8,8 +8,4 @@ class OGL_Command_With_OneToMany extends OGL_Command_With {
 	public function query_result($result) {
 		parent::query_result($result);
 	}
-
-	public function query_contrib($query) {
-		parent::query_contrib($query);
-	}
 }

fbe45601ae3e9b4ba97df1dc7d94b1f37300a44e
rlm80 <rlemaigr@yahoo.fr>
2009-11-29 10:52:26 +0000

    ogl_command_with_manytomany::query_contrib is now based on parent implementation of the method in ogl_command_with.

--- classes/ogl/command/with/manytomany.php
+++ classes/ogl/command/with/manytomany.php
@@ -13,51 +13,11 @@ class OGL_Command_With_ManyToMany extends OGL_Command_With {
 	}
 
 	public function query_contrib($query) {
-		// Gather data :
-		$rel		= $this->relationship;
-		$from_fk	= $rel->from_fk();
-		$to_fk		= $rel->to_fk();
-		$src_fields = $rel->from()->fields();
-		$piv_fields = $rel->pivot()->fields();
-		$trg_fields = $rel->to()->fields();
-		$src_table	= $rel->from()->table();
-		$piv_table	= $rel->pivot()->table();
-		$trg_table	= $rel->to()->table();
-		$src_alias	= $this->src_set->name;
-		$trg_alias	= $this->trg_set->name;
-		$piv_alias	= self::pivot_alias($src_alias, $trg_alias);
-
-		// init query with src table :
-		$query->from(array($src_table, $src_alias));
-
-		// join pivot table :
-		$query->join(array($piv_table, $piv_alias), 'INNER');
-		foreach($from_fk as $src_field => $piv_field)
-			$query->on($src_alias.'.'.$src_fields[$src_field]['column'],'=',$piv_alias.'.'.$piv_fields[$piv_field]['column']);
-
-		// join trg table :
-		$query->join(array($trg_table, $trg_alias), 'INNER');
-		foreach($to_fk as $trg_field => $piv_field)
-			$query->on($piv_alias.'.'.$piv_fields[$piv_field]['column'],'=',$trg_alias.'.'.$trg_fields[$trg_field]['column']);
-
-		// Add requested pivot fields :
-		foreach ($piv_fields as $name => $field)
-			if(isset($this->pivot_fields[$name]))
-				$query->select(array($piv_alias.'.'.$field['column'], $piv_alias.':'.$name));
-
-		// Add requested trg fields :
-		foreach ($trg_fields as $name => $field)
-			if(isset($this->trg_fields[$name]))
-				$query->select(array($trg_alias.'.'.$field['column'], $trg_alias.':'.$name));
-
-		// Apply db builder calls :
-		$this->apply_calls($query);
+		parent::query_contrib($query);
+		$pivot_alias = OGL_Relationship_ManyToMany::pivot_alias($this->src_set->name, $this->trg_set->name);
+		$this->relationship->pivot()->add_fields($query, $this->pivot_fields, $pivot_alias);
 	}
 
 	public function query_result($result) {
-	}
-
-	protected static function pivot_alias($src_alias, $trg_alias) {
-		return $src_alias.'__'.$trg_alias;
 	}
 }

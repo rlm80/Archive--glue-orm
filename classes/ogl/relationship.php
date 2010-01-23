504aa732c8498e932b3acdd5c28a32af93985600
rlm80 <rlemaigr@yahoo.fr>
2010-01-23 19:11:36 +0000

    still simplifying relationships

--- classes/ogl/relationship.php
+++ classes/ogl/relationship.php
@@ -1,42 +1,8 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-/*
- * IDEE : introduire variable $reverse, avec pour valeur par défaut...un truc basé sur to().
- *
- * relationships doivent bien être des classes plutôt que des propriétés des entité chargées
- * via init(), car ainsi seul le code des relations qui sont effectivement utilisée est compilé
- */
-/*
- * Relationships link entities to each others. They are objects responsible for
- * handling the conversion between database foreing keys, and model objects
- * references. They know about pivot tables involved in relationships and the way
- * primary key columns in one table map to foreign key columns in another.
- *
- * Relationships are always requested from OGL by name :
- * - by referring to them in an OGL query (ogl::load('users u')->with(u.postS p)),
- * - ...
- *
- * Relationship objects behave as singletons. When an relationship object is required by name,
- * OGL will instanciate :
- * a) the class OGL_Relationship_<entity name>_<relationship name> if it exists,
- * b) one of the default class OGL_Relationship_OneToMany, OGL_Relationship_ManyToOne,
- * OGL_Relationship_OneToOne or OGL_Relationship_ManyToMany, depending on the relationship
- * name, if no such class is found.
- *
- * Conventions used by the default OGL_Relationships classes :
- * - ...
- *
- * If you wish to define your own ... you need to create your
- * own OGL_Relationship_<entity name>_<relationship name> relationship class.
- */
-
 abstract class OGL_Relationship {
 	// Relationships cache :
 	static protected $relationships = array();
-
-	// Cardinality values :
-	const SINGLE	= 1; // Relationship associates at most one target entity instance to each source entity instance
-	const MULTIPLE	= 2; // Relationship may associate more than one target entity instance to each source entity instance
 
 	// Properties that may NOT be set in children classes :
 	public $from;
@@ -46,29 +12,30 @@ abstract class OGL_Relationship {
 	public $to;
 	public $property;
 	public $reverse;
-	public $cardinality;
+	public $multiple;
+	public $mapping;
 
 	protected function __construct($from, $name) {
 		// Set properties :
-		$this->from = OGL_entity::get($from);
+		$this->from = $from;
 		$this->name = $name;
 
 		// Init properties :
-		if ( ! isset($this->to))			$this->to			= $this->default_to();
-		if ( ! isset($this->property))		$this->property		= $this->default_property();
-		if ( ! isset($this->reverse))		$this->reverse		= $this->default_reverse();
-		if ( ! isset($this->cardinality))	$this->cardinality	= $this->default_cardinality();
-		$this->to		= OGL_Entity::get($this->to);
-		$this->reverse	= OGL_Relationship::get($this->to->name, $this->reverse);
-	}
+		if ( ! isset($this->to))		$this->to		= $this->default_to();
+		if ( ! isset($this->property))	$this->property	= $this->default_property();
+		if ( ! isset($this->reverse))	$this->reverse	= $this->default_reverse();
+		if ( ! isset($this->multiple))	$this->multiple	= $this->default_multiple();
+		if ( ! isset($this->mapping))	$this->mapping	= $this->default_mapping();
 
-	abstract public function add_joins($query, $src_alias, $trg_alias);
+		// Turn properties into objects where appropriate :
+		$this->reverse	= OGL_Relationship::get($this->to, $this->reverse);
+		$this->from		= OGL_entity::get($this->from);
+		$this->to		= OGL_Entity::get($this->to);
+	}
 
 	protected function default_to() {
 		switch (substr($this->name, -1)) {
-			case 'Z': $to = substr($this->name, 0, -1); break;
-			case 'S': $to = substr($this->name, 0, -1); break;
-			case '1': $to = substr($this->name, 0, -1); break;
+			case 'Z': case 'S': case '1': $to = substr($this->name, 0, -1); break;
 			default : $to = $this->name;
 		}
 		return $to;
@@ -76,10 +43,10 @@ abstract class OGL_Relationship {
 
 	protected function default_reverse() {
 		switch (substr($this->name, -1)) {
-			case 'Z': $reverse = $this->from->name.'Z';	break;
-			case 'S': $reverse = $this->from->name;		break;
-			case '1': $reverse = $this->from->name.'1';	break;
-			default : $reverse = $this->from->name.'S';
+			case 'Z': $reverse = $this->from.'Z';	break;
+			case 'S': $reverse = $this->from;		break;
+			case '1': $reverse = $this->from.'1';	break;
+			default : $reverse = $this->from.'S';
 		}
 		return $reverse;
 	}
@@ -88,14 +55,36 @@ abstract class OGL_Relationship {
 		return $this->name;
 	}
 
-	protected function default_cardinality() {
+	protected function default_multiple() {
 		switch (substr($this->name, -1)) {
-			case 'Z': $cardinality = self::MULTIPLE;	break;
-			case 'S': $cardinality = self::MULTIPLE;	break;
-			case '1': $cardinality = self::SINGLE;		break;
-			default : $cardinality = self::SINGLE;
+			case 'Z': case 'S': $multiple = true; break;
+			default : $multiple = false;
 		}
-		return $cardinality;
+		return $multiple;
+	}
+
+	protected function default_mapping() {
+		$mapping = array();
+		$from	 = OGL_entity::get($this->from);
+		$to		 = OGL_Entity::get($this->to);
+		switch (substr($this->name, -1)) {
+			case 'Z':
+				$pivot = ($this->from < $this->to) ? $this->from.'2'.$this->to : $this->to.'2'.$this->from;
+				foreach($from->default_fk as $src => $trg) $mapping[$src] = $pivot.'.'.$trg;
+				foreach(array_flip($to->default_fk) as $src => $trg) $mapping[$pivot.'.'.$src] = $trg;
+				break;
+			case 'S': $mapping = $from->default_fk; break;
+			case '1': $pk = array_values($from->pk); $mapping = array_combine($pk, $pk); break;
+			default : $mapping = array_flip($to->default_fk);
+		}
+		return $mapping;
+	}
+
+	public function add_joins($query, $src_alias, $trg_alias) {
+		self::join($query, $src_alias, $this->from, $trg_alias, $this->to, $this->fk);
+
+		self::join($query, $src_alias,   $this->from,  $pivot_alias, $this->pivot, $this->fk1);
+		self::join($query, $pivot_alias, $this->pivot, $trg_alias,   $this->to,    $this->fk2);
 	}
 
 	public function load_relationships($result, $src_alias, $trg_alias)	{
@@ -105,11 +94,11 @@ abstract class OGL_Relationship {
 		foreach($result as $row) {
 			if (isset($row[$src_key])) {
 				$src = $row[$src_key];
-				if ($this->cardinality === self::MULTIPLE && ! isset($src->$property))
+				if ($this->multiple && ! isset($src->$property))
 					$src->$property = array();
 				if (isset($row[$trg_key])) {
 					$trg = $row[$trg_key];
-					if ($this->cardinality === self::MULTIPLE)
+					if ($this->multiple)
 						$src->$property[spl_object_hash($trg)] = $trg;
 					else
 						$src->$property = $trg;
@@ -131,13 +120,10 @@ abstract class OGL_Relationship {
 	// the name and the available classes.
 	static protected function create($entity_name, $name) {
 		$class = 'OGL_Relationship_'.ucfirst($entity_name).'_'.ucfirst($name);
-		if ( ! class_exists($class)) {
-			if (substr($name, -1) == 'Z')
-				$class = 'OGL_Relationship_Indirect';
-			else
-				$class = 'OGL_Relationship_Direct';
-		}
-		$relationship = new $class($entity_name, $name);
+		if (class_exists($class))
+			$relationship = new $class($entity_name, $name);
+		else
+			$relationship = new self($entity_name, $name);
 		return $relationship;
 	}
 
@@ -149,5 +135,4 @@ abstract class OGL_Relationship {
 		foreach($fk as $src_field => $trg_field)
 			$query->on($trg_alias.'.'.$trg_fields[$trg_field]['column'], '=', $src_alias.'.'.$src_fields[$src_field]['column']);
 	}
-
 }

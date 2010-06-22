f1149c5bb288a2a75014fb96f6aae019dbe9fb33
rlm80 <rlemaigr@yahoo.fr>
2010-06-23 01:05:19 +0200

    moved variables unsetting into proxy class constructor

--- views/glue_proxy.php
+++ views/glue_proxy.php
@@ -15,12 +15,43 @@
 	- magic functions : __get to implement lazy loading of fields and properties.
 */
 
+<?php
+	// Prepare what's needed for parent class constructor override :
+	$rc = new ReflectionClass($model_class);
+	if ($rc->hasMethod('__construct')) {
+		$has_constructor = true;
+		$rm = new ReflectionMethod($model_class, '__construct');
+		foreach($rm->getParameters() as $parm) {
+			$sig_parms[] = ($parm->isPassedByReference() ? '&' : '') .
+						   '$' . $parm->getName() .
+						   ($parm->isDefaultValueAvailable() ? '=' . var_export($parm->getDefaultValue(), true) : '');
+			$call_parms[] =	'$' . $parm->getName();
+		}
+		$constructor_sig_parms = implode(', ', $sig_parms);
+		$constructor_call_parms = implode(', ', $call_parms);
+	}
+	else
+		$has_constructor = false;
+?>
+
 class <?php echo $proxy_class ?> extends <?php echo $model_class ?> {
 	// Static properties :
 	static public $glue_entity		= <?php var_export($entity)		?>;	// Entity name
 	static public $glue_properties	= <?php var_export($properties)	?>;	// Fields => properties mapping
 	static public $glue_types		= <?php var_export($types)		?>;	// Fields => property types mapping
-	static public $glue_lazy_props	= <?php var_export($lazy_props)	?>;	// Lazy-loadable properties
+
+	// Constructor :
+	public function __construct(<?php echo $has_constructor ? $constructor_sig_parms : '' ?>) {
+		// Unset variables :
+		foreach(get_class_vars() as $var => $default)
+			if (is_null($default))
+				unset($this->$var);
+
+		<?php if ($has_constructor) { ?>
+		// Call parent constructor :
+		parent::__construct(<?php echo $constructor_call_parms; ?>);
+		<?php } ?>
+	}
 
 	// Entity mapper :
 	static public function glue_entity() { return glue::entity(self::$glue_entity); }
@@ -66,26 +97,6 @@ class <?php echo $proxy_class ?> extends <?php echo $model_class ?> {
 		return $values;
 	}
 
-	// Mass lazy-loadable properties unsetter :
-	static public function glue_unset($objects) {
-		// Turn parameters into arrays if they are not already :
-		if ( ! is_array($objects)) $objects = array($objects);
-			
-		// Loop on objects :
-		foreach($objects as $obj) {
-			// Variables to unset :
-			if (isset(self::$lazy_props))
-				$vars = array_intersect_keys(array_keys(get_object_vars($obj)), self::$lazy_props);
-			else
-				$vars = array_keys(get_object_vars($obj));
-
-			// Unset variables :
-			foreach($vars as $var)
-				if (is_null($obj->$var))
-					unset($obj->$var);
-		}
-	}
-
 	// Active Record features :
 	public function delete() { return self::glue_entity()->delete($this); }
 	public function insert() { return self::glue_entity()->insert($this); }
@@ -93,10 +104,7 @@ class <?php echo $proxy_class ?> extends <?php echo $model_class ?> {
 
 	// Lazy loading of properties and relationships :
 	public function __get($var) {
-		if ( ! isset(self::$lazy_props) || in_array($var, self::$lazy_props)) {
-			$this->glue_entity()->proxy_lazy_load($this, $var);
-			return $this->$var;
-		}
-		return parent::__get($var);
+		$this->glue_entity()->proxy_lazy_load($this, $var);
+		return $this->$var;
 	}
 }
